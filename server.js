218e964b34efeeba5945e9fb525ad852e62b531c
srivastava-aryan <aru93295@gmail.com>
2026-07-06 02:49:48 +0530

    changed port for nodemailer

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ const PendingMessage = mongoose.model('PendingMessage', pendingSchema);
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
